123295cdae1f659aba55fcc94887423fd2552416
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2023-06-20 20:38:32 -0300

    Introdução manipular string

---
+++ strings.py
@@ -0,0 +1,13 @@
+s = "banana";
+ss = 'banana';
+
+print("primeiro variavel: ", s, id(s));
+
+print("segundo variavel: ", ss);
+
+s = s.replace("b", "z");
+
+print("Substituir: ", s);
+
+print("Primeira letra: " , s[0]);
+print("Ultimo letra: " , s[-1]);
