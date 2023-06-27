5d37e117ba3c6df6e3d4d209a7fa02708baa3063
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2023-06-27 10:44:30 -0300

    adicionei concatenado tupla e criei variavel valor

--- ListaTuplasDicionario/tuplas.py
+++ ListaTuplasDicionario/tuplas.py
@@ -1,16 +1,31 @@
-tupla1 = (1,2,3,4,5);
-tupla2 = ('I', 'N', 'T', 'E', 'R');
-tupla3 = (1, 'python', True);
+tuplaNumeros = (1,2,3,4,5);
+tuplaLetras = ('I', 'N', 'T', 'E', 'R');
+tuplaMistos = (1, 'python', True);
 set = {1,2,3,4,5};
 lista = [1,2,3,4,5,6,7,8];
-print(tupla1);
-print(tupla2);
-print(tupla3);
-print(type(tupla1));
-print(type(set));
-print(type(lista));
+print(tuplaNumeros);
+print(tuplaLetras);
+print(tuplaMistos);
+print("Variavel 'tuplaNumeros' para ver qual é tipo: ", type(tuplaNumeros));
+print("Variavel 'set' para ver qual é tipo: ", type(set));
+print("Variavel 'lista' para ver qual é tipo: ", type(lista));
 
 # Acessando itens individualmente em tuplas
-print(tupla2[1]);
-tupla2[1] = "J";
+# print(tupla2[1]);
+# tupla2[1] = "J";
 
+# Fatiamento de itens na tupla
+print(tuplaNumeros[1:4]);
+
+# Concatenando tupla
+tuplasNumero1 = 1,2,3;
+tuplasNumero2 = 4,5,6;
+tuplasConcatenado = tuplasNumero1 + tuplasNumero2;
+print("Concatenando tuplas: ", tuplasConcatenado);
+
+# Criando variaveis novas com os valores de uma tupla
+a, b, c = tuplasNumero2;
+print("Valores das variaveis: ");
+print(a);
+print(b);
+print(c);
