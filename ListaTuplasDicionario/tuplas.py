d82126f13fcfb34bae97175f828d26d7f2d42c96
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2023-06-26 21:42:31 -0300

    criei classe dicionario e inicio

---
+++ ListaTuplasDicionario/tuplas.py
@@ -0,0 +1,16 @@
+tupla1 = (1,2,3,4,5);
+tupla2 = ('I', 'N', 'T', 'E', 'R');
+tupla3 = (1, 'python', True);
+set = {1,2,3,4,5};
+lista = [1,2,3,4,5,6,7,8];
+print(tupla1);
+print(tupla2);
+print(tupla3);
+print(type(tupla1));
+print(type(set));
+print(type(lista));
+
+# Acessando itens individualmente em tuplas
+print(tupla2[1]);
+tupla2[1] = "J";
+
