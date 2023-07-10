09e5ec44ffeb457c3a2bfd99d175c818c5858c08
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2023-07-10 19:38:00 -0300

    pausada por causa tenho duvida alguma coisa

---
+++ Exercicios/exercicio4.py
@@ -0,0 +1,49 @@
+# 1-Utilizando um loop "while", imprima os números de 1 a 10.
+# quantidade = 1
+# while quantidade <= 10:
+#   print(quantidade);
+#   quantidade += 1;
+# print("===================");
+# 2- Utilizando um loop "for", imprima os números de 1 a 10.
+# for quantidade in range(1, 11):
+#   print(quantidade);
+# print("===================");
+# 3- Utilizando um loop "while", calcule a soma dos números de 1 a 100.
+# contador = 1;
+# calculeSoma = 0;
+# while contador != 101:
+#     calculeSoma = calculeSoma + contador;
+#     contador += 1;
+# print("Resultado soma: ", calculeSoma);
+# print("===================");
+# 4- Utilizando um loop "for", calcule a soma dos números de 1 a 100.
+# calculeSoma = 0;
+# for soma in range(1,101):
+#     calculeSoma += soma;
+# print("SOMA:", calculeSoma);
+# print("===================");
+# 5- Utilizando um loop "while", imprima os números pares de 1 a 20.
+# contador = 1;
+# while contador < 21:
+#     if contador % 2 == 0 :
+#         print(contador);
+#     contador += 1;
+# print("===================");
+# 6- Utilizando um loop "for", imprima os números pares de 1 a 20.
+# for numero in range(1,21):
+#     if numero % 2 == 0 :
+#         print(numero);
+print("===================");
+# 7- Utilizando um loop "while", inverta uma string digitada pelo usuário.
+
+print("===================");
+# 8- Utilizando um loop "for", verifique se uma palavra digitada pelo usuário é 
+# um palíndromo (lê-se da mesma forma de trás para frente)
+
+print("===================");
+# 9- Utilizando um loop "while", encontre o menor número inteiro cujo quadrado seja maior do que 1000.
+
+print("===================");
+# 10- Utilizando um loop "for", imprima os elementos de uma lista em ordem inversa.
+
+print("===================");
